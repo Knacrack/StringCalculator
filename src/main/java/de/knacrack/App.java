252c5a88f21798ac32f00ef47df983797cdda0f7
Knacrack <knacrack@gmail.com>
2023-02-10 20:36:54 +0100

    Initial commit

---
+++ src/main/java/de/knacrack/App.java
@@ -0,0 +1,13 @@
+package de.knacrack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
