6deb328ed437e7dcf0f4c670f99b2b04960496d5
Knacrack <knacrack@gmail.com>
2023-02-11 01:00:19 +0100

    Teilen des Strings
    add: Die Funktion "splitCalculation" nimmt den String der berechnet werden soll und teilt ihn in eine LinkedList um das spätere verarbeiten zu vereinfachen

--- src/main/java/de/knacrack/App.java
+++ src/main/java/de/knacrack/App.java
@@ -1,13 +1,25 @@
 package de.knacrack;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class App {
+
+    public static void main(String[] args) {
+        String toCalculate = "4+5-20+44-3*sin2^3*2*(2+1-3)*21^1";
+        System.out.println(splitCalculation(toCalculate));
+    }
+
+    private static LinkedList<String> splitCalculation(String calculation) {
+        Pattern pattern = Pattern.compile("[0-9]+|(\\+|-|\\*)+|[A-Za-z]+|[!^/:]+");
+        Matcher matcher = pattern.matcher(calculation);
+        LinkedList<String> parts = new LinkedList<>();
+
+        while (matcher.find()) {
+            parts.add(matcher.group());
+        }
+
+        return parts;
     }
 }
